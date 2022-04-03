4b240548ef3e3b6bcec04a81c1e5e81241916b40
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-04-03 14:14:04 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,6 @@ $(function() {
     });
 });
 
-// Window size
-// $(function() {
-//     $(window).bind('load resize', function() {
-//         topOffset = 50;
-//         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-//         if (width < 768) {
-//             $('nav.navbar-collapse').addClass('collapse');
-//             topOffset = 100;
-//         } else {
-//             $('nav.navbar-collapse').removeClass('collapse');
-//         };
-
-//         height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-//         height = height - topOffset;
-//         if (height < 1) height = 1;
-//         if (height > topOffset) {
-//             $('.sidebar-nav').css('min-height', (height) + 'px');
-//         };
-//     });
-// });
-
 // Fading the jQuery loader
 jQuery(window).on('load', function() {
     // Hide the matched elements by fading them to transparent.
